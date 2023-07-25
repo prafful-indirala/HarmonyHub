2f8af73566ace63418d5700d7b37518f44582c0e
Prafful Indirala <prafful.log@gmail.com>
2023-07-25 21:45:23 +0100

    AppIntroScreen

---
+++ components/AppIntroScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default AppIntroScreen = () => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>AppIntro Screen</Text>
+    </View>
+  );
+};
