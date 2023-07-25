c81dec84846e4cac79de69caf9a89ce941193118
Prafful Indirala <prafful.log@gmail.com>
2023-07-25 21:45:07 +0100

    SignUpScreen

---
+++ components/SignUpScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default SignUpScreen = () => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>SignUp Screen</Text>
+    </View>
+  );
+};
