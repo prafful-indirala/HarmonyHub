14db72a9e6e19e5aa0e06091284372896473e6fe
Prafful Indirala <prafful.log@gmail.com>
2023-07-25 21:45:01 +0100

    SplashScreen

---
+++ components/SplashScreen.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+export default SplashScreen = () => {
+  const navigation = useNavigation();
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate("Welcome");
+    }, 3000);
+  });
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#4874e8",
+      }}
+    >
+      <Text style={{ color: "#fff", fontWeight: "900", fontSize: 30 }}>
+        TeamMate
+      </Text>
+      <View style={{ position: "absolute", bottom: 80 }}>
+        <ActivityIndicator size={"large"} height={30} color={"#fff"} />
+      </View>
+    </View>
+  );
+};
